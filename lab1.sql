4cba4b3c939dbd50a81093708a0a924c47b87245
Anton Leonov <grmmr07@gmail.com>
2014-10-21 20:29:10 +0400

    labs modified

--- lab1.sql
+++ lab1.sql
@@ -1,14 +1,13 @@
 CREATE TABLE adresses
-(build_number NUMERIC(6) CONSTRAINT pk_home PRIMARY KEY,
+(build_number NUMERIC(6) PRIMARY KEY,
 street_name VARCHAR(100) NOT NULL,
 home_number NUMERIC(4) );
 
 CREATE TABLE owners
-(id NUMERIC(6) CONSTRAINT pk_id PRIMARY KEY,
+(id NUMERIC(6) PRIMARY KEY,
 fullname VARCHAR(100) NOT NULL,
 birthdate DATE NOT NULL,
 sex CHAR(1) NOT NULL );
-
 
 CREATE TABLE documents
 (id NUMERIC(4) CONSTRAINT ref_id REFERENCES owners(id),
@@ -18,31 +17,9 @@ doc_num NUMERIC(6) NOT NULL,
 who_gave VARCHAR(100) NOT NULL );
 
 CREATE TABLE ownage
-(owner NUMERIC(6) CONSTRAINT ref_owner REFERENCES owners(id),
-build_number NUMERIC(6) CONSTRAINT ref_build_number REFERENCES adresses(build_number),
+(owner NUMERIC(6) REFERENCES owners(id),
+build_number NUMERIC(6) REFERENCES adresses(build_number),
 flat_number NUMERIC(4) NOT NULL,
-part NUMERIC(8) NOT NULL CONSTRAINT check_part CHECK(part<=1),
+part NUMERIC(8,2) NOT NULL, CHECK(part<=1),
 ownage_start DATE NOT NULL,
 ownage_finish DATE );
-
-INSERT INTO adresses (BUILD_NUMBER, STREET_NAME, HOME_NUMBER)
-VALUES ('3','KASHIRSKAYA','14');
-
-INSERT INTO owners (ID, FULLNAME, BIRTHDATE, SEX)
-VALUES ('123','CHRISTINA','01.01.1980','f');
-
-INSERT INTO ownage (OWNER, BUILD_NUMBER, FLAT_NUMBER, PART, OWNAGE_START, OWNAGE_FINISH)
-VALUES ('123', '3', '403', '1', '01.03.1997', '10.05.2009') ;
-
-SELECT owner, build_number, flat_number
-FROM ownage
-WHERE part=1
-ORDER BY owner;
-
-SELECT id, doc_type, doc_ser, doc_num, who_gave
-FROM docements
-ORDER BY id;
-
-SELECT flat_number, owner
-FROM ownage
-WHERE 
--- lab2.sql
+++ lab2.sql
@@ -1,7 +1,7 @@
 --1--
-SELECT owners.fullname, ownage.flat_number
+SELECT owners.fullname, ownage.build_number
 FROM owners, ownage
-WHERE ownage.part = 1
+WHERE ownage.part = 1 AND ownage.owner = owners.id
 ORDER BY owner;
 --2--
 SELECT owners.fullname, documents.*
@@ -9,14 +9,14 @@ FROM owners, documents
 WHERE documents.id = owners.id;
 --2--
 SELECT owners.fullname, documents.*
-FROM owners FULL OUTER JOIN documents ON documents.id = owners.id;
+FROM owners LEFT JOIN documents ON documents.id = owners.id
+UNION
+SELECT owners.fullname, documents.*
+FROM documents RIGHT JOIN owners ON documents.id = owners.id;
 --3--
 SELECT ownage.flat_number, count(*)
 FROM ownage, owners
-WHERE ownage.owner = owners.id
-
-
-;
+WHERE ownage.owner = owners.id;
 --4--
 SELECT DISTINCT ownage.flat_number
 FROM ownage, adresses
